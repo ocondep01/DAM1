8a507df287702253deae6e293132c59a9b33cb9d
ocondep01 <ocondep01@aula1-08>
2015-10-07 20:05:47 +0200

    Subo primer ejemplo en Java

---
+++ EjerciciosJava/src/ejerciciosjava/EjerciciosJava.java
@@ -0,0 +1,21 @@
+package ejerciciosjava;
+
+/**
+ *
+ * @author ocondep01
+ */
+public class EjerciciosJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        String nombre="Omar Conde";
+        int edad=21;
+        double altura=1.75;
+        System.out.println("Me llamo "+ nombre + ", tengo "+ edad+ " años, y mido "+ altura);
+        // TODO code application logic here
+    }
+    
+}
